2ba05f51c8818e6154896795f384219639faa88a
Maria Verkina <maria.verkina@mir.dev>
2022-07-13 15:01:32 +0300

    login gpage checks implemented implemented

--- test_main_page.py
+++ test_main_page.py
@@ -1,22 +1,27 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
 
+link = "http://selenium1py.pythonanywhere.com/"
+
+
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
 
 
 def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+
+
 
 
 

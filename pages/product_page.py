def952e47beb56b793b5c252008b78c9f8b60676
Maria Verkina <maria.verkina@mir.dev>
2022-07-14 17:45:53 +0300

    tests for product page (on success msg; on login link)

--- pages/product_page.py
+++ pages/product_page.py
@@ -13,12 +13,15 @@ class ProductPage(BasePage):
     def find_product_price_on_page(self):
         self.page_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE_ON_PAGE).text
 
-    def should_add_product_to_basket(self):
-        self.find_product_name_on_page()
-        self.find_product_price_on_page()
+    def add_to_basket(self):
         add_to_basket_btn = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BTN)
         add_to_basket_btn.click()
         self.solve_quiz_and_get_code()
+
+    def should_add_product_to_basket(self):
+        self.find_product_name_on_page()
+        self.find_product_price_on_page()
+        self.add_to_basket()
         self.should_product_is_added()
 
     def is_name_in_msgs(self, ):
@@ -44,3 +47,11 @@ class ProductPage(BasePage):
     def should_product_is_added(self):
         self.should_names_be_same()
         self.should_price_be_same()
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+           "Success message is presented, but should not be"
+
+    def should_disappear_success_message(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+           "Success message did not disappear"
